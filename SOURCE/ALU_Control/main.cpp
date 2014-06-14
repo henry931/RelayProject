cd65c398bd105c950d05acc5a680a94923eca918
Henry Poulton <hgp10@ic.ac.uk>
2014-06-14 20:17:40 +0100

    Finished load program

--- SOURCE/ALU_Control/main.cpp
+++ SOURCE/ALU_Control/main.cpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <string>
 #include <bitset>
+#include <stdexcept>
 
 // Pi Headers
 #ifdef __ARMEL__
@@ -20,9 +21,6 @@ static int verbose;
 
 class ALU {
 
-	// Critical delay in seconds to allow relays to switch 
-	static const unsigned ClockPeriod = unsigned(0.5);
-
 	// Serial IO Base Pins
 	static const unsigned IC1 = 100;
 	static const unsigned IC2 = 200;
@@ -31,6 +29,11 @@ class ALU {
 	static const unsigned ADDR_A[8], ADDR_B[8], ADDR_R[8], ADDR_I[3], ADDR_Carry;
 
 public:
+
+	// Clock frequency Hz
+	double ClockFrequency{ 60 };
+	// Critical delay in microseconds to allow relays to switch 
+	unsigned ClockPeriod = unsigned(1000000 / ClockFrequency);
 
 	// Variables for input and output
 	uint8_t A, B, Instruction, Result, CarryOut;
@@ -86,7 +89,7 @@ public:
 		for (unsigned i = 0; i < 3; i++) digitalWrite(ADDR_I[i], Instruction & (1 << i));
 
 		// Wait for relays
-		sleep(ClockPeriod);
+		usleep(ClockPeriod);
 
 		// Read In Result
 		Result = 0;
@@ -194,7 +197,7 @@ public:
 			return -1;
 		}
 
-		if (verbose = 1)
+		if (verbose > 0)
 		{
 			const std::bitset<8> result_bits(Result), carry_bits(CarryOut), a_bits(A), b_bits(B), instruction_bits(Instruction);
 			std::cout << "OK" << std::endl;
@@ -218,21 +221,32 @@ const unsigned ALU::ADDR_R[8] = { IC2 + 8, IC2 + 7, IC2 + 6, IC2 + 11, IC2 + 10,
 const unsigned ALU::ADDR_I[3] = { IC1 + 1, IC1 + 0, IC1 + 2 };
 const unsigned ALU::ADDR_Carry = IC2 + 12;
 
-int LoadProgram(const std::vector<uint16_t> &MEMORY, const std::string &SourceFile)
+int LoadProgram(std::vector<uint16_t> &MEMORY, const std::string &SourceFile)
 {
+	typedef struct {
+		bool LabelDefinition;
+		bool LabelledInstruction;
+		bool PlainInstruction;
+		// Opcode & corresponding mnemonic
+		uint16_t Opcode;
+		std::string Mnemonic;
+		// Address & corresponding label
+		uint16_t Address;
+		std::string Label;
+		// Literal and corresponding destination
+		uint16_t Literal;
+		std::string Destination;
+	} buffer_mem_t;
+
 	typedef struct {
 		size_t starts;
 		size_t ends;
 	} argument_t;
 
-	// Labelled indicated a label is used as to indicate the address
-	// Instruction indicates that the line is an instruction
-	std::vector<bool> Labelled(MEMORY_LEN, 0), Instruction(MEMORY_LEN, 0);
+	std::vector<buffer_mem_t> MemoryBuff;
 
 	// Isolate specific mnemonics for binary conversion
 	std::string line, label, opcode, destination;
-
-	unsigned linecount{};
 
 	std::ifstream ifs(SourceFile);
 	if (!ifs.is_open())
@@ -250,7 +264,7 @@ int LoadProgram(const std::vector<uint16_t> &MEMORY, const std::string &SourceFi
 		std::vector<bool> whitespace(linelength, 1);
 		for (size_t i{}; i < linelength; i++) if (line.compare(i, 1, " ", 1)) whitespace[i] = 0;
 
-		// Find arguments
+		// Find arguments by identifying whitespace boundaries
 		bool midargument{};
 		//unsigned argumentsfound{};
 		std::vector<argument_t> arguments;
@@ -275,30 +289,174 @@ int LoadProgram(const std::vector<uint16_t> &MEMORY, const std::string &SourceFi
 			if ((!whitespace[i - 1]) & whitespace[i])
 			{
 				midargument = 0;
-				arguments.back().ends = i-1;
+				arguments.back().ends = i - 1;
 			}
 		}
 		//Last char
 		if (midargument) arguments.back().ends = linelength - 1;
 
 		const std::size_t colonpos = line.find(':');
+		if (colonpos != line.rfind(':'))
+		{
+			// Complain
+		}
 		if (colonpos == size_t(-1))
 		{
-			// No label found
+			// No label found...
+			// Must be a plain instruction
+
+			// Add to memory buffer
+			// First argument is mnemonic
+			// Second argument is destination
+			if (arguments.size() == 2)
+			{
+				MemoryBuff.push_back(buffer_mem_t{ 0, 0, 1 }); // See struct definition
+				MemoryBuff.back().Mnemonic = line.substr(arguments[0].starts, arguments[0].ends - arguments[0].starts + 1);
+				MemoryBuff.back().Destination = line.substr(arguments[1].starts, arguments[1].ends - arguments[1].starts + 1);
+			}
+			else if (arguments.size() == 1)
+			{
+				MemoryBuff.push_back(buffer_mem_t{ 0, 0, 1 }); // See struct definition
+				MemoryBuff.back().Mnemonic = line.substr(arguments[0].starts, arguments[0].ends - arguments[0].starts + 1);
+			}
+			else
+			{
+				// Must have two arguments
+				// Throw tantrum
+			}
+
 		}
 		else
-			
 		{
 			// Label found
-			Labelled[linecount] = 1;
 
-			for (unsigned i = 0; i < colonpos; i++) __nop;
+			if ((colonpos != arguments[0].ends) && (colonpos != arguments[1].starts))
+			{
+				// Colon in forbidden position
+				// Error
+			}
+
+			//Remove colon reference
+			if (colonpos == arguments[0].ends) arguments[0].ends--;
+			else if (colonpos == arguments[1].starts)
+			{
+				// if argument just a colon - delete, else increment
+				if (arguments[1].starts == arguments[1].ends) arguments.erase(arguments.begin() + 1);
+				else arguments[1].starts++;
+			}
+
+			if (arguments.size() == 2)
+			{
+				// Just a label definition
+				MemoryBuff.push_back(buffer_mem_t{ 1, 0, 0 });
+				MemoryBuff.back().Label = line.substr(arguments[0].starts, arguments[0].ends - arguments[0].starts + 1);
+				MemoryBuff.back().Destination = line.substr(arguments[1].starts, arguments[1].ends - arguments[1].starts + 1);
+			}
+			else if (arguments.size() == 3)
+			{
+				// Instruction with label
+				MemoryBuff.push_back(buffer_mem_t{ 0, 1, 0 });
+				MemoryBuff.back().Label = line.substr(arguments[0].starts, arguments[0].ends - arguments[0].starts + 1);
+				MemoryBuff.back().Mnemonic = line.substr(arguments[1].starts, arguments[1].ends - arguments[1].starts + 1);
+				MemoryBuff.back().Destination = line.substr(arguments[2].starts, arguments[2].ends - arguments[2].starts + 1);
+			}
+			else
+			{
+				// Throw tantrum
+			}
 		}
-
-		linecount++;
 	}
 
+	// Close file
 	ifs.close();
+
+	// Process Memory Buffer	
+	for (size_t iter{}; iter < MemoryBuff.size(); iter++)
+	{
+		// Instructions can be converted straight to binary
+		if (!MemoryBuff[iter].Mnemonic.compare("LDA")) MemoryBuff[iter].Opcode = 0;
+		else if (!MemoryBuff[iter].Mnemonic.compare("STO")) MemoryBuff[iter].Opcode = 1;
+		else if (!MemoryBuff[iter].Mnemonic.compare("ADD")) MemoryBuff[iter].Opcode = 2;
+		else if (!MemoryBuff[iter].Mnemonic.compare("SUB")) MemoryBuff[iter].Opcode = 3;
+		else if (!MemoryBuff[iter].Mnemonic.compare("JMP")) MemoryBuff[iter].Opcode = 4;
+		else if (!MemoryBuff[iter].Mnemonic.compare("JGE")) MemoryBuff[iter].Opcode = 5;
+		else if (!MemoryBuff[iter].Mnemonic.compare("JNE")) MemoryBuff[iter].Opcode = 6;
+		else if (!MemoryBuff[iter].Mnemonic.compare("STP")) MemoryBuff[iter].Opcode = 7;
+		//else if (!MemoryBuff[iter].Mnemonic.compare("ADD")) MemoryBuff[iter].Opcode = 8;
+		else if (!MemoryBuff[iter].Mnemonic.compare("INC")) MemoryBuff[iter].Opcode = 9;
+		else if (!MemoryBuff[iter].Mnemonic.compare("XOR")) MemoryBuff[iter].Opcode = 10;
+		else if (!MemoryBuff[iter].Mnemonic.compare("AND")) MemoryBuff[iter].Opcode = 11;
+		else if (!MemoryBuff[iter].Mnemonic.compare("XNR")) MemoryBuff[iter].Opcode = 12;
+		else if (!MemoryBuff[iter].Mnemonic.compare("LOR")) MemoryBuff[iter].Opcode = 13;
+		else if (!MemoryBuff[iter].Mnemonic.compare("NOT")) MemoryBuff[iter].Opcode = 14;
+		else if (!MemoryBuff[iter].Mnemonic.compare("ROL")) MemoryBuff[iter].Opcode = 15;
+
+		// Fill in adresses
+		MemoryBuff[iter].Address = uint16_t(iter);
+	}
+
+	// Match up labels and convert to addresses
+	for (size_t iter{}; iter < MemoryBuff.size(); iter++)
+	{
+		// If an instruction
+		if (MemoryBuff[iter].LabelledInstruction || MemoryBuff[iter].PlainInstruction)
+		{
+			// If stop opcode, just set literal to zero
+			if (MemoryBuff[iter].Opcode == 7) MemoryBuff[iter].Literal = 0;
+			else
+			{
+				// Try to convert to int (test for literal)
+				try
+				{
+					MemoryBuff[iter].Literal = stoi(MemoryBuff[iter].Destination);
+				}
+				catch (const std::invalid_argument &ia)
+				{
+					std::cout << "Not a literal. Looking for label... " << ia.what() << std::endl;
+					for (size_t inner{}; inner < MemoryBuff.size(); inner++)
+					{
+						// Find label
+						if (!MemoryBuff[iter].Destination.compare(MemoryBuff[inner].Label))
+						{
+							MemoryBuff[iter].Literal = MemoryBuff[inner].Address;
+							break;
+						}
+						// Label not found
+						// throw tantrum
+					}
+				}
+			}
+		}
+
+		// If not an instruction
+		if (MemoryBuff[iter].LabelDefinition)
+		{
+			// Try to convert to int (test for literal)
+			try
+			{
+				MemoryBuff[iter].Literal = stoi(MemoryBuff[iter].Destination);
+			}
+			catch (const std::invalid_argument &ia)
+			{
+				std::cout << "Not a literal. Definition error" << ia.what() << std::endl;
+				// throw tantrum
+			}
+		}
+	}
+
+	// Finally load the actual memory with binary
+	for (size_t iter{}; iter < MemoryBuff.size(); iter++)
+	{
+		// Set whole word to literal
+		MEMORY.push_back(MemoryBuff[iter].Literal);
+
+		// If an instruction the four MSBs are the opcode
+		if (MemoryBuff[iter].LabelledInstruction || MemoryBuff[iter].PlainInstruction)
+		{
+			MEMORY.back() = (MEMORY.back() & 4095) | (MemoryBuff[iter].Opcode << 12);
+		}
+	}
+	
 	return 0;
 }
 
@@ -318,14 +476,21 @@ int TemplateInstruction(ALU *RelayALU, void (ALU::*ClockALU)(), std::vector<uint
 
 int main(int argc, char* argv[])
 {
-	if (argc>1)
+	if (argc > 1)
 	{
-		try { verbose = std::stoi(argv[1]); }
-		catch (const std::invalid_argument& ia) { verbose = 0; }
+		try
+		{
+			verbose = std::stoi(argv[1]);
+		}
+		catch (const std::invalid_argument &ia)
+		{
+			std::cout << "Invalid argument for verbose level" << ia.what() << std::endl;
+			verbose = 0;
+		}
 	}
 
 	// Processor memory 12-bit addressing
-	std::vector<uint16_t> MEMORY(MEMORY_LEN, 0);
+	std::vector<uint16_t> MEMORY;
 
 	// Finished Variable
 	bool finished = 0;
@@ -347,6 +512,14 @@ int main(int argc, char* argv[])
 	const std::string SourceFile = "input.txt";
 	LoadProgram(MEMORY, SourceFile);
 
+	// Check memory usage
+	std::cout << "Memory usage is " << MEMORY.size() * 2 << " out of 8192 bytes" << std::endl;
+	if (MEMORY.size() >= MEMORY_LEN)
+	{
+		// Program too large
+		std::cout << "Program size too large! Exiting..." << std::endl;
+	}
+
 	while ((!finished) && (PC < MEMORY_LEN))
 	{
 		// Get instruction
@@ -354,7 +527,7 @@ int main(int argc, char* argv[])
 
 		// Extract opcode & address
 		opcode = IR >> 12;
-		address = IR & (MEMORY_LEN-1);
+		address = IR & (MEMORY_LEN - 1);
 
 		//Execute Instruction
 		switch (opcode) {
